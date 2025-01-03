1a6c13b02ecd004c8e39b69498deeb82afbe2cde
xyxrnd <rendhirichardo1@unsub.ac.id>
2025-01-03 14:23:59 +0700

    DASHBOARD

---
+++ app/Views/admin/home.php
@@ -0,0 +1,9 @@
+<?= $this->extend('layouts/base') ?>
+<?= $this->section('title') ?>Dashboard<?= $this->endSection() ?>
+<?= $this->section('content') ?>
+<div class="container-fluid p-0">
+
+    <h1 class="h3 mb-3"><strong>Analytics</strong> Dashboard</h1>
+
+</div>
+<?= $this->endSection() ?>
